d011b7ce09bce53902f45abbe0d8bd4b1676d4f4
Arjan de Pooter <mail@arjandepooter.nl>
2020-12-01 08:53:40 +0100

    Day 1

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,13 +1,38 @@
 const INPUT: &'static str = include_str!("./input.txt");
 
-type Solution = u32;
+type Solution = i32;
+
+fn parse_input(data: &str) -> Vec<i32> {
+    data.lines().filter_map(|line| line.parse().ok()).collect()
+}
+
+fn find_sum(vec: &Vec<i32>, n: i32) -> Option<(i32, i32)> {
+    vec.into_iter()
+        .find(|entry| vec.contains(&(n - *entry)))
+        .map(|entry| (*entry, n - entry))
+}
+
+fn find_triplet(vec: &Vec<i32>, n: i32) -> Option<(i32, i32, i32)> {
+    (0..vec.len()).find_map(|idx| {
+        let mut cloned = vec.clone();
+        let entry = cloned.swap_remove(idx);
+
+        find_sum(&cloned, n - entry).map(|(a, b)| (a, b, entry))
+    })
+}
 
 fn solve_a(data: &str) -> Solution {
-    todo!()
+    let entries = parse_input(data);
+    let (a, b) = find_sum(&entries, 2020).unwrap();
+
+    a * b
 }
 
 fn solve_b(data: &str) -> Solution {
-    todo!()
+    let entries = parse_input(data);
+    let (a, b, c) = find_triplet(&entries, 2020).unwrap();
+
+    a * b * c
 }
 
 fn main() {
@@ -19,13 +44,20 @@ fn main() {
 mod tests {
     use super::*;
 
+    const EXAMPLE: &str = "1721
+979
+366
+299
+675
+1456";
+
     #[test]
     fn examples_a() {
-        assert_eq!(solve_a(""), 0);
+        assert_eq!(solve_a(EXAMPLE), 514579);
     }
 
     #[test]
     fn examples_b() {
-        assert_eq!(solve_b(""), 0);
+        assert_eq!(solve_b(EXAMPLE), 241861950);
     }
 }
