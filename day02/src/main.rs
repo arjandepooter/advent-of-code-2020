587af85a7d452efd9b8a0e338a2417a46678a0f5
Arjan de Pooter <mail@arjandepooter.nl>
2020-12-02 08:53:00 +0100

    Solved day 2

---
+++ day02/src/main.rs
@@ -0,0 +1,90 @@
+#![feature(str_split_once)]
+use std::str::FromStr;
+
+const INPUT: &'static str = include_str!("./input.txt");
+
+type Solution = usize;
+
+struct Password {
+    password: String,
+    start: usize,
+    stop: usize,
+    char: char,
+}
+
+struct ParseError;
+
+impl FromStr for Password {
+    type Err = ParseError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let (policy, password) = s.split_once(": ").ok_or(ParseError)?;
+        let (range, s_char) = policy.split_once(' ').ok_or(ParseError)?;
+        let (s_start, s_stop) = range.split_once('-').ok_or(ParseError)?;
+        let start = s_start.parse::<usize>().map_err(|_| ParseError)?;
+        let stop = s_stop.parse::<usize>().map_err(|_| ParseError)?;
+        let char = s_char.chars().next().ok_or(ParseError)?;
+
+        Ok(Password {
+            password: password.to_string(),
+            char,
+            start,
+            stop,
+        })
+    }
+}
+
+impl Password {
+    fn is_valid(&self) -> bool {
+        let count = self.password.chars().filter(|c| *c == self.char).count();
+        (self.start..=self.stop).contains(&count)
+    }
+
+    fn is_valid_b(&self) -> bool {
+        let first = self.password.chars().nth(self.start - 1).unwrap() == self.char;
+        let second = self.password.chars().nth(self.stop - 1).unwrap() == self.char;
+
+        first ^ second
+    }
+}
+
+fn parse_lines(data: &'static str) -> impl Iterator<Item = Password> {
+    data.lines()
+        .filter_map(|line| line.parse::<Password>().ok())
+}
+
+fn solve_a(data: &'static str) -> Solution {
+    let passwords = parse_lines(data);
+
+    passwords.filter(Password::is_valid).count()
+}
+
+fn solve_b(data: &'static str) -> Solution {
+    let passwords = parse_lines(data);
+
+    passwords.filter(Password::is_valid_b).count()
+}
+
+fn main() {
+    println!("Part 1: {}", solve_a(INPUT));
+    println!("Part 2: {}", solve_b(INPUT));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const EXAMPLE: &str = "1-3 a: abcde
+1-3 b: cdefg
+2-9 c: ccccccccc";
+
+    #[test]
+    fn examples_a() {
+        assert_eq!(solve_a(EXAMPLE), 2);
+    }
+
+    #[test]
+    fn examples_b() {
+        assert_eq!(solve_b(EXAMPLE), 1);
+    }
+}
