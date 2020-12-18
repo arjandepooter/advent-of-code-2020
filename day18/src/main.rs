5043279a1b483f9908c4387863ea460afd42d3d5
Arjan de Pooter <mail@arjandepooter.nl>
2020-12-18 16:07:31 +0100

    Solved day 18

--- day18/src/main.rs
+++ day18/src/main.rs
@@ -11,65 +11,117 @@ fn parse_input(input: &str) -> Data {
     input.lines().collect()
 }
 
-#[derive(Debug, Clone, Copy, PartialEq)]
-enum StackEntry {
-    Mul,
-    Add,
-    OpenBracket,
-    Value(u64),
-}
-use StackEntry::*;
+trait Operator: std::fmt::Debug {
+    fn evaluate(&self, lhs: u64, rhs: u64) -> u64;
 
-fn push_value(stack: &mut Vec<StackEntry>, value: u64) {
-    match stack.last() {
-        Some(Mul | Add) => {
-            let op = stack.pop().unwrap();
-            let lhs = stack.pop().unwrap();
-            stack.push(Value(match (lhs, op) {
-                (Value(lhs), Add) => value + lhs,
-                (Value(lhs), Mul) => value * lhs,
-                _ => 0,
-            }))
-        }
-        _ => stack.push(Value(value)),
+    fn get_precedence(&self) -> u64 {
+        0
+    }
+}
+#[derive(Debug)]
+struct Add;
+#[derive(Debug)]
+struct Mul;
+
+impl Operator for Add {
+    fn evaluate(&self, lhs: u64, rhs: u64) -> u64 {
+        lhs + rhs
     }
 }
 
-fn evaluate_expression(expr: &str) -> u64 {
-    let mut stack = vec![];
+impl Operator for Mul {
+    fn evaluate(&self, lhs: u64, rhs: u64) -> u64 {
+        lhs * rhs
+    }
+}
 
-    for c in expr.chars() {
+#[derive(Debug)]
+struct PrioAdd;
+
+impl Operator for PrioAdd {
+    fn evaluate(&self, lhs: u64, rhs: u64) -> u64 {
+        lhs + rhs
+    }
+
+    fn get_precedence(&self) -> u64 {
+        10
+    }
+}
+
+#[derive(Debug)]
+enum OpEntry<'a> {
+    Operator(&'a Box<dyn Operator>),
+    Bracket,
+}
+
+fn pop_operator(output_stack: &mut Vec<u64>, op_stack: &mut Vec<OpEntry>) {
+    let rhs = output_stack.pop().unwrap();
+    let lhs = output_stack.pop().unwrap();
+    let operator = op_stack.pop().unwrap();
+
+    if let OpEntry::Operator(operator) = operator {
+        output_stack.push(operator.evaluate(lhs, rhs));
+    }
+}
+
+fn evaluate(s: &str, operators: &HashMap<char, Box<dyn Operator>>) -> u64 {
+    let mut output_stack = vec![];
+    let mut op_stack: Vec<OpEntry> = vec![];
+
+    for c in s.chars() {
         match c {
-            '(' => stack.push(OpenBracket),
-            ')' => {
-                let val = stack.pop().unwrap();
-                stack.pop();
-                if let Value(val) = val {
-                    push_value(&mut stack, val);
-                }
-            }
-            '*' => stack.push(Mul),
-            '+' => stack.push(Add),
+            ' ' => {}
             '0'..='9' => {
-                let digit = c.to_digit(10).unwrap();
-                push_value(&mut stack, digit as u64);
+                output_stack.push(c.to_digit(10).unwrap() as u64);
             }
-            _ => (),
+            c if operators.contains_key(&c) => {
+                let cur_op = operators.get(&c).unwrap();
+
+                while let Some(OpEntry::Operator(prev_op)) = op_stack.last() {
+                    if prev_op.get_precedence() >= cur_op.get_precedence() {
+                        pop_operator(&mut output_stack, &mut op_stack);
+                    } else {
+                        break;
+                    }
+                }
+                op_stack.push(OpEntry::Operator(cur_op));
+            }
+            '(' => op_stack.push(OpEntry::Bracket),
+            ')' => loop {
+                match op_stack.last() {
+                    Some(OpEntry::Bracket) => {
+                        op_stack.pop();
+                        break;
+                    }
+                    Some(_) => pop_operator(&mut output_stack, &mut op_stack),
+                    _ => break,
+                }
+            },
+            c => panic!("INVALID CHAR: {}", c),
         }
     }
 
-    match stack.get(0) {
-        Some(Value(n)) => *n,
-        _ => 0,
+    while op_stack.len() > 0 {
+        pop_operator(&mut output_stack, &mut op_stack);
     }
+
+    output_stack[0]
 }
 
 fn solve_a(data: &Data) -> Solution {
-    data.iter().map(|s| evaluate_expression(*s)).sum()
+    let mut operators: HashMap<char, Box<dyn Operator>> = HashMap::new();
+    operators.insert('+', Box::new(Add));
+    operators.insert('*', Box::new(Mul));
+
+    data.iter().map(|s| evaluate(*s, &operators)).sum()
 }
 
-fn solve_b(_data: &Data) -> Solution {
-    todo!()
+fn solve_b(data: &Data) -> Solution {
+    let mut operators: HashMap<char, Box<dyn Operator>> = HashMap::new();
+    operators.insert('+', Box::new(PrioAdd));
+    operators.insert('*', Box::new(Mul));
+
+    data.iter().map(|s| evaluate(*s, &operators)).sum()
 }
 
 fn main() {
@@ -84,9 +136,23 @@ mod tests {
     use super::*;
 
     #[test]
-    fn evaluate() {
-        let expr = "3 * 7 * (4 + 3)";
+    fn solve_a() {
+        let mut operators: HashMap<char, Box<dyn Operator>> = HashMap::new();
+        operators.insert('+', Box::new(Add));
+        operators.insert('*', Box::new(Mul));
 
-        assert_eq!(evaluate_expression(expr), 147);
+        assert_eq!(evaluate("5 + (8 * 3 + 9 + 3 * 4 * 3)", &operators), 437);
+    }
+
+    #[test]
+    fn solve_b() {
+        let mut operators: HashMap<char, Box<dyn Operator>> = HashMap::new();
+        operators.insert('+', Box::new(PrioAdd));
+        operators.insert('*', Box::new(Mul));
+
+        assert_eq!(
+            evaluate("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))", &operators),
+            669060
+        );
     }
 }
